02bf3d38787bdb32a67172c66fd731d8850fcd37
Ina91 <ina.met91@gmail.com>
2020-07-07 17:21:06 +0200

    prima impostazione condizioni

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
+// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+// il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// va applicato uno sconto del 20% per i minorenni
+// va applicato uno sconto del 40% per gli over 65
+
+
 var distanza = parseInt(prompt('inserisci distanza che vuoi percorrere in km'));
-console.log(distanza);
-var costo = distanza * (0,21);
+var costo = distanza * (0.21);
 var eta = parseInt(prompt('inserisci la tua età'));
+if (eta<18) {
+    costo = (distanza * 0.21)*0.8;
+
+}else if (eta>65) {
+    costo = (distanza * 0.21)*0.6;
+}
 document.getElementById('costo').innerHTML = costo;
